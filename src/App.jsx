5940060037eeac9ee04423b67600fe289e3e6994
PieStw <piero.stalteri@icloud.com>
2025-01-23 10:11:35 +0100

    aggiunta rotta Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { GlobalProvider } from "./contex/GlobalContex";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import DefaultLayout from "./layout/DefaultLayout";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route Component={DefaultLayout}>
-              <Route path="/" element={}></Route>
-              <Route path="*" element={}></Route>
+              <Route path="/" element={<HomePage />}></Route>
+              <Route path="*" element={""}></Route>
             </Route>
           </Routes>
         </BrowserRouter>
