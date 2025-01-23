c629b386f50bf2a11d09214ed42c3916f2ad8733
PieStw <piero.stalteri@icloud.com>
2025-01-23 10:35:29 +0100

    aggiunta estetica card

--- src/components/propertyCard/PropertyCard.jsx
+++ src/components/propertyCard/PropertyCard.jsx
@@ -1,13 +1,14 @@
 export default function PropertyCard({ property }) {
   return (
-    <div className="card">
-      <img src="..." className="card-img-top" alt="..." />
+    <div className="card p-0 h-100">
+      <img
+        src={property.immagine}
+        className="card-img-top"
+        alt={property.immagine}
+      />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <h5 className="card-title">{property.nome}</h5>
+        <p className="card-text">{property.indirizzo}</p>
         <a href="#" className="btn btn-primary">
           Go somewhere
         </a>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { useGlobalContex } from "../contex/GlobalContex";
+import PropertyCard from "../components/propertyCard/PropertyCard";
 
 export default function HomePage() {
   const { propertyList } = useGlobalContex();
 
   return (
-    <div>
-      {propertyList.map((property) => (
-        <PropertyCard key={property.id} property={property} />
-      ))}
+    <div className="container">
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {propertyList.map((property) => (
+          <div className="col" key={property.id}>
+            <PropertyCard property={property} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
