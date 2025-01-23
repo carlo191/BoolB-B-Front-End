e4b4d9f3d51d18da751364b445d3e510eae41b71
Carlo Elvisi <carloelvisi01@outlook.it>
2025-01-23 11:44:42 +0100

    css per la navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,36 +1,31 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
-export default function NavBar() {
+import { Link, NavLink } from "react-router-dom";
+export default function Navbar() {
   return (
-    <>
-      <>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Navbar
+    <nav className="navbar navbar-expand-lg bg-primary">
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="#">
+          
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a href="/" className="nav-link text-white">
+            <i className="fa-light fa-house fa-2xl">Homepage</i>
             </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <NavLink to="/" className={"nav-link active"}>
-                  HomePage
-                </NavLink>
-              </div>
-            </div>
           </div>
-        </nav>
-      </>
-    </>
+        </div>
+      </div>
+    </nav>
   );
 }
