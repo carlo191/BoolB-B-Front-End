999d5fe8bf5e7b458749e9498e7c5443db1fdc8c
AndreaBeltramin <andreabeltra99@gmail.com>
2025-01-23 17:17:56 +0100

    change hearts of cards

--- src/components/propertyCard/PropertyCard.jsx
+++ src/components/propertyCard/PropertyCard.jsx
@@ -6,7 +6,13 @@ export default function PropertyCard({ property }) {
 				className="card-img-top h-100 "
 				alt={property.immagine}
 			/>
-			<i className="fa-solid fa-heart fa-2xl heart"></i>
+
+			<a className="btn btn-primary heart-btn">
+				<i className="fa-regular fa-heart fa-2xl heart"></i>
+				<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+					{property.numero_like}
+				</span>
+			</a>
 			<div className="card-body">
 				<h5 className="card-title">{property.nome}</h5>
 				<p className="card-text">{property.indirizzo}</p>
