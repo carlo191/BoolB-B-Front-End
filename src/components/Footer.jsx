3e57892a36e071bc82262fc44cac2941d8d8b7c3
Carlo Elvisi <carloelvisi01@outlook.it>
2025-01-24 11:03:14 +0100

    fix footer e review list

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 
-
 export default function Footer() {
   return (
     <footer className="bg-primary text-white py-3">
       <ul className="d-flex justify-content-center list-unstyled mb-3">
         <li className="mx-3">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-white">
+          <a
+            href="https://facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white"
+          >
             <i className="fa-brands fa-facebook fa-xl"></i>
           </a>
         </li>
         <li className="mx-3">
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white">
+          <a
+            href="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white"
+          >
             <i className="fa-brands fa-instagram fa-xl"></i>
           </a>
         </li>
         <li className="mx-3">
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-white">
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white"
+          >
             <i className="fa-brands fa-linkedin fa-xl"></i>
           </a>
         </li>
       </ul>
-      <div className="d-flex justify-content-center">
-        <p className="mb-0">@2025</p>
-      </div>
     </footer>
   );
 }
