6c993d102b855b46385c2f0de295e2a3f5bfc03a
Carlo Elvisi <carloelvisi01@outlook.it>
2025-01-24 10:54:45 +0100

    review list e review item

---
+++ src/components/Review/ReviewList.jsx
@@ -0,0 +1,10 @@
+import ReviewItem from "./ReviewItem";
+export default function ReviewList({ reviews }) {
+    return (
+      <div className="row">
+        {reviews.map((review) => (
+          <ReviewItem review={review} key={review.review_id} />
+        ))}
+      </div>
+    );
+  }
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useGlobalContext } from "../contex/GlobalContext";
 import { useEffect } from "react";
+import ReviewList from "../components/Review/ReviewList";
 
 export default function DetailPage() {
   const { id } = useParams();
@@ -13,14 +14,20 @@ export default function DetailPage() {
       <div className="container">
         <div className="row">
           <div className="col">
-            <img src={property.immagine} alt={property.immagine} />
+            <img
+              className="img-fluid"
+              src={property.immagine}
+              alt={property.immagine}
+            />
           </div>
           <div className="col">
             <h1>{property.nome}</h1>
             <p>{property.indirizzo}</p>
             <p>{property.descrizione}</p>
             <p>{property.numero_like}</p>
-            <p>ciao</p>
+            <div>
+              <ReviewList reviews={review} />
+            </div>
           </div>
         </div>
       </div>
