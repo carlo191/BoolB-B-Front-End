ac98722595ab848e94371a75803541cf12eaf419
SimoneChiodo <simochi.005@gmail.com>
2025-01-24 12:16:02 +0100

    aggiunto card css

--- src/components/propertyCard/PropertyCard.jsx
+++ src/components/propertyCard/PropertyCard.jsx
@@ -2,26 +2,25 @@ import { Link } from "react-router-dom";
 
 export default function PropertyCard({ property }) {
   return (
-    <div className="card p-0 h-100">
-      <img
-        src={property.immagine}
-        className="card-img-top h-100 "
-        alt={property.immagine}
-      />
+    <Link to={`/property/${property.id}`}>
+      <div className="card property-card p-0">
+        <img
+          src={property.immagine}
+          className="card-img-top position-relative"
+          alt={property.immagine}
+        />
 
-      <a className="btn btn-primary heart-btn">
-        <i className="fa-regular fa-heart fa-2xl heart"></i>
-        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-          {property.numero_like}
-        </span>
-      </a>
-      <div className="card-body">
-        <h5 className="card-title">{property.nome}</h5>
-        <p className="card-text">{property.indirizzo}</p>
-        <Link className="btn btn-primary" to={`/property/${property.id}`}>
-          Vedi dettaglio
-        </Link>
+        <a className="btn btn-primary heart-btn">
+          <i className="fa-regular fa-heart fa-2xl heart"></i>
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {property.numero_like}
+          </span>
+        </a>
+        <div className="card-body">
+          <h5 className="card-title">{property.nome}</h5>
+          <p className="card-text">{property.indirizzo}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
