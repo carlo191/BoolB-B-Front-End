d6114126b889d53b0d73707967c850e5184475a1
SimoneChiodo <simochi.005@gmail.com>
2025-01-24 12:56:58 +0100

    change heart style

--- src/components/propertyCard/PropertyCard.jsx
+++ src/components/propertyCard/PropertyCard.jsx
@@ -1,21 +1,34 @@
 import { Link } from "react-router-dom";
 
 export default function PropertyCard({ property }) {
+  const handleHeartClick = (event) => {
+    event.stopPropagation(); // Impedisce che l'evento si propaghi al genitore
+    event.preventDefault(); // Impedisce il comportamento predefinito del Link
+
+    // onLike(property.id); // Chiama una funzione per mettere like
+  };
+
   return (
     <Link to={`/property/${property.id}`}>
-      <div className="card property-card p-0">
+      <div className="card property-card p-0 position-relative">
+        {/* Property Image */}
         <img
           src={property.immagine}
-          className="card-img-top position-relative"
+          className="card-img-top"
           alt={property.immagine}
         />
 
-        <a className="btn btn-primary heart-btn">
-          <i className="fa-regular fa-heart fa-2xl heart"></i>
-          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {property.numero_like}
-          </span>
-        </a>
+        {/* Heart */}
+        <span
+          className="position-absolute badge rounded-pill heart-pill text-bg-light fs-6 fw-semibold"
+          onClick={handleHeartClick}
+        >
+          <i className="fa-solid fa-heart fa-2xl heart" />
+          &nbsp;
+          {property.numero_like}
+        </span>
+
+        {/* Property Description */}
         <div className="card-body">
           <h5 className="card-title">{property.nome}</h5>
           <p className="card-text">{property.indirizzo}</p>
