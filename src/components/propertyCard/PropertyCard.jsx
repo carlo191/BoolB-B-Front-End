ca682884cf3d66cf13175311b8d7e0c103dc5a8b
PieStw <piero.stalteri@icloud.com>
2025-01-24 10:01:52 +0100

    modifica css

--- src/components/propertyCard/PropertyCard.jsx
+++ src/components/propertyCard/PropertyCard.jsx
@@ -1,25 +1,25 @@
 export default function PropertyCard({ property }) {
-	return (
-		<div className="card p-0 h-100">
-			<img
-				src={property.immagine}
-				className="card-img-top h-100 "
-				alt={property.immagine}
-			/>
+  return (
+    <div className="card p-0 h-100">
+      <img
+        src={property.immagine}
+        className="card-img-top h-100 "
+        alt={property.immagine}
+      />
 
-			<a className="btn btn-primary heart-btn">
-				<i className="fa-regular fa-heart fa-2xl heart"></i>
-				<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-					{property.numero_like}
-				</span>
-			</a>
-			<div className="card-body">
-				<h5 className="card-title">{property.nome}</h5>
-				<p className="card-text">{property.indirizzo}</p>
-				<a href="#" className="btn btn-primary">
-					Go somewhere
-				</a>
-			</div>
-		</div>
-	);
+      <a className="btn btn-primary heart-btn">
+        <i className="fa-regular fa-heart fa-2xl heart"></i>
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          {property.numero_like}
+        </span>
+      </a>
+      <div className="card-body">
+        <h5 className="card-title">{property.nome}</h5>
+        <p className="card-text">{property.indirizzo}</p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
 }
