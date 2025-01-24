7bccb6f3cd90b0f61ea0ecc92fd0fea5a091902b
PieStw <piero.stalteri@icloud.com>
2025-01-24 11:03:52 +0100

    fix footer

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 // Components
 import NavBar from "../components/navbar/Navbar";
-import Footer from "../components/Footer";
+import Footer from "../components/footer/Footer";
 
 export default function DefaultLayout() {
   return (
