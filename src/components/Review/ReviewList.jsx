dadb3355e216e3bcf148e9fef190680d50f0599a
PieStw <piero.stalteri@icloud.com>
2025-01-27 17:47:41 +0100

    aggiunta key

--- src/components/Review/ReviewList.jsx
+++ src/components/Review/ReviewList.jsx
@@ -5,7 +5,7 @@ export default function ReviewList({ reviews }) {
   return (
     <div className="row">
       {reviews.map((review) => (
-        <ReviewItem review={review} key={review.review_id} />
+        <ReviewItem review={review} key={review.id} />
       ))}
     </div>
   );
