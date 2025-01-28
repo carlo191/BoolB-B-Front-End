ef440ddee286cc04dbbfb55725b76d335afd902f
PieStw <piero.stalteri@icloud.com>
2025-01-28 11:39:05 +0100

    filtri avanzati completi

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,78 +1,77 @@
-// Global variables
 import PropertyCard from "../components/propertyCard/PropertyCard";
 import { useGlobalContext } from "../context/GlobalContext";
 import { useState, useEffect } from "react";
 
 export default function SearchPage() {
   const { propertyList, search, setSearch } = useGlobalContext();
-
   const [tipologia, setTipologia] = useState("");
-
-  const [filteredList, setFilteredList] = useState(propertyList);
+  const [stanze, setStanze] = useState("");
+  const [letti, setLetti] = useState("");
+  const [filteredList, setFilteredList] = useState([]);
 
   useEffect(() => {
-    if (tipologia === "null") {
-      setFilteredList(propertyList);
-      return;
-    }
-    const filtered = propertyList.filter((property) =>
-      property.tipologia.includes(tipologia)
+    const filtered = propertyList.filter(
+      (property) =>
+        property.tipologia.includes(tipologia) &&
+        property.indirizzo.toLowerCase().includes(search.toLowerCase()) &&
+        property.numero_stanze >= stanze &&
+        property.numero_letti >= letti
     );
     setFilteredList(filtered);
-  }, [tipologia]);
-
-  function handleSearch(e) {
-    setSearch(e.target.value);
-  }
-
-  function handleSelect(e) {
-    setTipologia(e.target.value);
-  }
+  }, [tipologia, search, propertyList, stanze, letti]);
 
   return (
-    <>
-      <div className="container">
-        <form className="navbar-form navbar-left mb-5">
-          <input
-            type="text"
-            className="form-control col-lg-8"
-            placeholder="Ricerca per città o via..."
-            value={search}
-            onChange={(e) => handleSearch(e)}
-          />
+    <div className="container">
+      <form
+        className="navbar-form navbar-left mb-5"
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <input
+          type="text"
+          className="form-control col-lg-8"
+          placeholder="Ricerca per città o via..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
 
-          <select
-            class="form-select"
-            aria-label="Default select example"
-            value={tipologia}
-            onChange={(e) => handleSelect(e)}
-          >
-            <option value="null" selected>
-              Tipologia
-            </option>
-            <option value="Appartamento">Appartamento</option>
-            <option value="Villa">Villa</option>
-            <option value="Attico">Attico</option>
-            <option value="Loft">Loft</option>
-            <option value="Monolocale">Monolocale</option>
-            <option value="Baita">Baita</option>
-            <option value="Casa indipendente">Casa indipendente</option>
-            <option value="Cottage">Cottage</option>
-            <option value="Residenza storica">Residenza storica</option>
-            <option value="Villetta a schiera">Villetta a schiera</option>
-          </select>
-        </form>
+        <select
+          className="form-select"
+          aria-label="Seleziona tipologia"
+          value={tipologia}
+          onChange={(e) => setTipologia(e.target.value)}
+        >
+          <option value="">Tipologia</option>
+          <option value="Appartamento">Appartamento</option>
+          <option value="Villa">Villa</option>
+          <option value="Attico">Attico</option>
+          <option value="Loft">Loft</option>
+          <option value="Monolocale">Monolocale</option>
+          <option value="Baita">Baita</option>
+          <option value="Casa indipendente">Casa indipendente</option>
+          <option value="Cottage">Cottage</option>
+          <option value="Residenza storica">Residenza storica</option>
+          <option value="Villetta a schiera">Villetta a schiera</option>
+        </select>
 
-        {filteredList.map(
-          (property) =>
-            property.indirizzo.toLowerCase().includes(search.toLowerCase()) && (
-              <PropertyCard
-                property={property}
-                key={property.id}
-              ></PropertyCard>
-            )
-        )}
-      </div>
-    </>
+        <input
+          type="number"
+          className="form-control col-lg-8"
+          placeholder="numero minimo stanze"
+          value={stanze}
+          onChange={(e) => setStanze(e.target.value)}
+        />
+        <input
+          type="number"
+          className="form-control col-lg-8"
+          placeholder="numero minimo posti letto"
+          value={letti}
+          onChange={(e) => setLetti(e.target.value)}
+        />
+      </form>
+
+      {filteredList.map((property) => (
+        <PropertyCard property={property} key={property.id}></PropertyCard>
+      ))}
+    </div>
   );
 }
