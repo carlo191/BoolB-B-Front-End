41606567aee2146a90af8d3867d1ee4455d531df
AndreaBeltramin <andreabeltra99@gmail.com>
2025-01-29 23:25:00 +0100

    fix img detailPage

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,26 +3,26 @@ export default function Footer() {
 		<div className="bg-primary text-white d-flex justify-content-between p-3">
 			<ul className="d-flex my-auto">
 				<li>
-					<i class="fa-regular fa-copyright"></i> 2025 BoolBnb, Inc.
+					<i className="fa-regular fa-copyright"></i> 2025 BoolBnb, Inc.
 				</li>
-				<li>&bull;</li>
+				<li className="d-none d-sm-block">&bull;</li>
 				<li>Privacy </li>
-				<li>&bull;</li>
+				<li className="d-none d-sm-block">&bull;</li>
 				<li>Termini</li>
-				<li>&bull;</li>
+				<li className="d-none d-sm-block">&bull;</li>
 				<li>Mappa del sito</li>
-				<li>&bull;</li>
+				<li className="d-none d-sm-block">&bull;</li>
 				<li>Dettagli dell'azienda</li>
 			</ul>
 
 			<ul className="d-flex my-auto">
-				<li className="me-2">
-					<i class="fa-solid fa-globe"></i> Italiano ( IT )
+				<li className="me-2 d-none d-md-block">
+					<i className="fa-solid fa-globe"></i> Italiano ( IT )
 				</li>
-				<li className="me-2">
-					<i class="fa-solid fa-euro-sign"></i> Eur
+				<li className="me-2 d-none d-md-block">
+					<i className="fa-solid fa-euro-sign"></i> Eur
 				</li>
-				<li>
+				<li className="d-none d-md-block">
 					<a
 						href="https://facebook.com"
 						target="_blank"
@@ -32,7 +32,7 @@ export default function Footer() {
 						<i className="fa-brands fa-facebook"></i>
 					</a>
 				</li>
-				<li>
+				<li className="d-none d-md-block">
 					<a
 						href="https://instagram.com"
 						target="_blank"
@@ -42,7 +42,7 @@ export default function Footer() {
 						<i className="fa-brands fa-instagram"></i>
 					</a>
 				</li>
-				<li>
+				<li className="d-none d-md-block">
 					<a
 						href="https://linkedin.com"
 						target="_blank"
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -9,99 +9,99 @@ import { useGlobalContext } from "../context/GlobalContext";
 import ReviewList from "../components/Review/ReviewList";
 
 export default function DetailPage() {
-  const { id } = useParams();
-  const { showProperty, property, updateProperty } = useGlobalContext();
+	const { id } = useParams();
+	const { showProperty, property, updateProperty } = useGlobalContext();
 
-  const handleHeartClick = (event) => {
-    event.stopPropagation();
-    event.preventDefault();
+	const handleHeartClick = (event) => {
+		event.stopPropagation();
+		event.preventDefault();
 
-    console.log(property);
+		console.log(property);
 
-    updateProperty({
-      ...property,
-      immagine: property.immagine.split("/").pop(),
-      numero_like: property.numero_like + 1,
-    });
-  };
+		updateProperty({
+			...property,
+			immagine: property.immagine.split("/").pop(),
+			numero_like: property.numero_like + 1,
+		});
+	};
 
-  // Load Data
-  useEffect(() => {
-    showProperty(id);
-  }, []);
+	// Load Data
+	useEffect(() => {
+		showProperty(id);
+	}, []);
 
-  return (
-    <>
-      <div className="container">
-        <div className="row">
-          {/* Property Image */}
-          <div className="col-12 col-sm-6 immagine-dettaglio">
-            <img
-              className="img-fluid "
-              src={property.immagine}
-              alt={property.immagine}
-            />
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+					{/* Property Image */}
+					<div className="col-12 col-sm-6 immagine-dettaglio">
+						<img
+							className="img-fluid "
+							src={property.immagine}
+							alt={property.immagine}
+						/>
 
-            {/* Badge */}
-            <div className="badge-dettaglio">
-              <Badge
-                tipologia={property.tipologia}
-                type={"primary"}
-                text={null}
-              />
-            </div>
+						{/* Badge */}
+						<span className="badge-dettaglio">
+							<Badge
+								tipologia={property.tipologia}
+								type={"primary"}
+								text={null}
+							/>
+						</span>
 
-            {/* Heart */}
-            <span
-              className="position-absolute badge rounded-pill heart-pill text-bg-light fs-6 fw-semibold me-2"
-              onClick={handleHeartClick}
-            >
-              <i className="fa-solid fa-heart fa-2xl heart" />
-              &nbsp;
-              {property.numero_like}
-            </span>
-          </div>
+						{/* Heart */}
+						<span
+							className="position-absolute badge rounded-pill heart-pill text-bg-light fs-6 fw-semibold me-2"
+							onClick={handleHeartClick}
+						>
+							<i className="fa-solid fa-heart fa-2xl heart" />
+							&nbsp;
+							{property.numero_like}
+						</span>
+					</div>
 
-          {/* Property Description */}
-          <div className="col-12 col-sm-6">
-            <ul>
-              <li>
-                <h1>{property.nome}</h1>
-              </li>
-              <li>
-                <strong>Indirizzo: </strong>
-                {property.indirizzo}
-              </li>
-              <li>
-                <strong>Numero stanze: </strong>
-                {property.numero_stanze}
-              </li>
-              <li>
-                <strong>Numero letti: </strong>
-                {property.numero_letti}
-              </li>
-              <li>
-                <strong>Numero bagni: </strong>
-                {property.numero_bagni}
-              </li>
-              <li>
-                <strong>Metri quadrati: </strong>
-                {property.metri_quadrati}
-              </li>
-              <li>
-                <strong>Email proprietario: </strong>
-                {property.email_proprietario}
-              </li>
-            </ul>
-          </div>
-        </div>
-        {/* Property Details */}
-        <div className="row mt-2">
-          <div>
-            <ReviewList reviews={property.recensioni} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+					{/* Property Description */}
+					<div className="col-12 col-sm-6">
+						<ul>
+							<li>
+								<h1>{property.nome}</h1>
+							</li>
+							<li>
+								<strong>Indirizzo: </strong>
+								{property.indirizzo}
+							</li>
+							<li>
+								<strong>Numero stanze: </strong>
+								{property.numero_stanze}
+							</li>
+							<li>
+								<strong>Numero letti: </strong>
+								{property.numero_letti}
+							</li>
+							<li>
+								<strong>Numero bagni: </strong>
+								{property.numero_bagni}
+							</li>
+							<li>
+								<strong>Metri quadrati: </strong>
+								{property.metri_quadrati}
+							</li>
+							<li>
+								<strong>Email proprietario: </strong>
+								{property.email_proprietario}
+							</li>
+						</ul>
+					</div>
+				</div>
+				{/* Property Details */}
+				<div className="row mt-2">
+					<div>
+						<ReviewList reviews={property.recensioni} />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
