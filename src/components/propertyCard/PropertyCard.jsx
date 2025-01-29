cbb21d16720097d8774e479de08ece9ca4caab81
PieStw <piero.stalteri@icloud.com>
2025-01-29 11:53:40 +0100

    modifica card

--- src/components/propertyCard/PropertyCard.jsx
+++ src/components/propertyCard/PropertyCard.jsx
@@ -2,66 +2,66 @@ import { Link } from "react-router-dom";
 import Badge from "../badge/Badge";
 
 export default function PropertyCard({ property }) {
-	const handleHeartClick = (event) => {
-		event.stopPropagation(); // Impedisce che l'evento si propaghi al genitore
-		event.preventDefault(); // Impedisce il comportamento predefinito del Link
+  const handleHeartClick = (event) => {
+    event.stopPropagation(); // Impedisce che l'evento si propaghi al genitore
+    event.preventDefault(); // Impedisce il comportamento predefinito del Link
 
-		// onLike(property.id); // Chiama una funzione per mettere like
-	};
+    // onLike(property.id); // Chiama una funzione per mettere like
+  };
 
-	return (
-		<Link to={`/property/${property.id}`}>
-			<div className="card property-card p-0 position-relative ">
-				{/* Property Image */}
-				<img
-					src={property.immagine}
-					className="card-img-top "
-					alt={property.immagine}
-				/>
-				{/* Badge */}
-				<span className="position-absolute top-5 start-0 mt-1 ms-2">
-					<Badge tipologia={property.tipologia} type={"primary"} text={null} />
-				</span>
+  return (
+    <Link to={`/property/${property.id}`}>
+      <div className="card property-card p-0 position-relative ">
+        {/* Property Image */}
+        <img
+          src={property.immagine}
+          className="card-img-top "
+          alt={property.immagine}
+        />
+        {/* Badge */}
+        <span className="position-absolute top-5 start-0 mt-1 ms-2">
+          <Badge tipologia={property.tipologia} type={"primary"} text={null} />
+        </span>
 
-				{/* Heart */}
-				<span
-					className="position-absolute badge rounded-pill heart-pill text-bg-light fs-6 fw-semibold"
-					onClick={handleHeartClick}
-				>
-					<i className="fa-solid fa-heart fa-2xl heart" />
-					&nbsp;
-					{property.numero_like}
-				</span>
+        {/* Heart */}
+        <span
+          className="position-absolute badge rounded-pill heart-pill text-bg-light fs-6 fw-semibold"
+          onClick={handleHeartClick}
+        >
+          <i className="fa-solid fa-heart fa-2xl heart" />
+          &nbsp;
+          {property.numero_like}
+        </span>
 
-				{/* Property Description */}
-				<div className="card-body h-100">
-					<h5 className="card-title">{property.nome}</h5>
-					<p className="card-text">{property.indirizzo}</p>
-					<div className="mt-2 row">
-						<div className="col-12">
-							<Badge
-								tipologia={property.numero_stanze}
-								type={"success"}
-								text={"Numero di stanze: "}
-							/>
-						</div>
-						<div className="col-12">
-							<Badge
-								tipologia={property.numero_letti}
-								type={"success"}
-								text={"Numero di posti letto: "}
-							/>
-						</div>
-						<div className="col-12">
-							<Badge
-								tipologia={property.numero_bagni}
-								type={"success"}
-								text={"Numero di bagni: "}
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-		</Link>
-	);
+        {/* Property Description */}
+        <div className="card-body h-100">
+          <h5 className="card-title">{property.nome}</h5>
+          <p className="card-text">{property.indirizzo}</p>
+          <div className="mt-2 row">
+            <div className="col-2 me-2 p-0">
+              <Badge
+                tipologia={property.numero_stanze}
+                type={"success"}
+                text={"Stanze: "}
+              />
+            </div>
+            <div className="col-2 me-2 p-0">
+              <Badge
+                tipologia={property.numero_letti}
+                type={"success"}
+                text={"Letti: "}
+              />
+            </div>
+            <div className="col-2 me-2 p-0">
+              <Badge
+                tipologia={property.numero_bagni}
+                type={"success"}
+                text={"Bagni: "}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
 }
