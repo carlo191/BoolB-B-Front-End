0fef0dbdad700531acd819f31cf001baefe3b095
AndreaBeltramin <andreabeltra99@gmail.com>
2025-01-29 10:06:10 +0100

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,51 +1,48 @@
-import React from "react";
-
 export default function Footer() {
-  return (
-    <div className="bg-primary text-white py-3">
-      {/* Upper Row */}
-      <ul className="d-flex justify-content-center list-unstyled mb-3">
-        {/* Facebook */}
-        <li className="mx-3">
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white"
-          >
-            <i className="fa-brands fa-facebook fa-xl"></i>
-          </a>
-        </li>
+	return (
+		<div className="bg-primary text-white py-3">
+			{/* Upper Row */}
+			<ul className="d-flex justify-content-center list-unstyled mb-3">
+				{/* Facebook */}
+				<li className="mx-3">
+					<a
+						href="https://facebook.com"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-white"
+					>
+						<i className="fa-brands fa-facebook fa-xl"></i>
+					</a>
+				</li>
+				{/* Instagram */}
+				<li className="mx-3">
+					<a
+						href="https://instagram.com"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-white"
+					>
+						<i className="fa-brands fa-instagram fa-xl"></i>
+					</a>
+				</li>
 
-        {/* Instagram */}
-        <li className="mx-3">
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white"
-          >
-            <i className="fa-brands fa-instagram fa-xl"></i>
-          </a>
-        </li>
+				{/* Linkedin */}
+				<li className="mx-3">
+					<a
+						href="https://linkedin.com"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-white"
+					>
+						<i className="fa-brands fa-linkedin fa-xl"></i>
+					</a>
+				</li>
+			</ul>
 
-        {/* Linkedin */}
-        <li className="mx-3">
-          <a
-            href="https://linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white"
-          >
-            <i className="fa-brands fa-linkedin fa-xl"></i>
-          </a>
-        </li>
-      </ul>
-
-      {/* Lower Row */}
-      <div className="d-flex justify-content-center">
-        <p className="mb-0">@2025</p>
-      </div>
-    </div>
-  );
+			{/* Lower Row */}
+			<div className="d-flex justify-content-center">
+				<p className="mb-0">@2025</p>
+			</div>
+		</div>
+	);
 }
