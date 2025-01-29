26fba5da86db93290bd1bd1c1f6e7975cf5235b4
PieStw <piero.stalteri@icloud.com>
2025-01-29 10:44:27 +0100

    search bar aggiustata

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,57 +7,59 @@ import { useGlobalContext } from "../context/GlobalContext";
 import PropertyCard from "../components/propertyCard/PropertyCard";
 
 export default function HomePage() {
-	const { propertyList, search, setSearch } = useGlobalContext();
-	const navigate = useNavigate();
+  const { propertyList, search, setSearch } = useGlobalContext();
+  const navigate = useNavigate();
 
-	function handleSearch(e) {
-		setSearch({ isActived: true, value: e.target.value });
-	}
+  function handleSearch(e) {
+    setSearch({ isActived: true, value: e.target.value });
+  }
 
-	function handleSubmit(e) {
-		e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
 
-		if (search.value === "") {
-			return;
-		} else {
-			navigate(`/search`);
-		}
-	}
+    if (search.value === "") {
+      return;
+    } else {
+      navigate(`/search`);
+    }
+  }
 
-	return (
-		<div className="container">
-			{/* SearchBar */}
-			<h2>Inizia la tua ricerca inserendo una città</h2>
-			<form
-				className="navbar-form navbar-left mb-5"
-				onSubmit={(e) => handleSubmit(e)}
-			>
-				<input
-					type="text"
-					className="form-control col-lg-8"
-					placeholder="..."
-					value={search.value}
-					onChange={(e) => handleSearch(e)}
-				/>
+  return (
+    <div className="container">
+      {/* SearchBar */}
+      <h2>Ricerca il tuo immobile in modo facile e con un click</h2>
+      <form className="navbar-form navbar-left mb-5" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-10">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Inserisci una città o una via"
+              value={search.value}
+              onChange={handleSearch}
+            />
+          </div>
+          <div className="col-2">
+            <button type="submit" className="btn btn-primary w-100">
+              Vai alla ricerca avanzata
+            </button>
+          </div>
+        </div>
+      </form>
 
-				<button type="submit" className="btn btn-primary mt-2">
-					Vai alla ricerca avanzata
-				</button>
-			</form>
+      {/* Property List */}
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {propertyList.map((property) => (
+          <div className="col" key={property.id}>
+            <PropertyCard property={property} />
+          </div>
+        ))}
+      </div>
 
-			{/* Property List */}
-			<div className="row row-cols-1 row-cols-md-3 g-4">
-				{propertyList.map((property) => (
-					<div className="col" key={property.id}>
-						<PropertyCard property={property} />
-					</div>
-				))}
-			</div>
-
-			<div className="text-center mt-5">
-				<h2> Continua a esplorare le nostre soluzioni immobiliari</h2>
-				<button className="btn btn-primary">Mostra altro</button>
-			</div>
-		</div>
-	);
+      <div className="text-center mt-5">
+        <h2> Continua a esplorare le nostre soluzioni immobiliari</h2>
+        <button className="btn btn-primary">Mostra altro</button>
+      </div>
+    </div>
+  );
 }
