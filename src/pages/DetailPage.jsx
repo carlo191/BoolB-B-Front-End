a4eddb100db84545c5daee85b63e7670438baa82
AndreaBeltramin <andreabeltra99@gmail.com>
2025-01-29 09:56:38 +0100

    modifche stile homepage

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Badge from "../components/badge/badge";
+import Badge from "../components/badge/badge.jsx";
 
 // Global variables
 import { useGlobalContext } from "../context/GlobalContext";
@@ -9,41 +9,54 @@ import { useGlobalContext } from "../context/GlobalContext";
 import ReviewList from "../components/Review/ReviewList";
 
 export default function DetailPage() {
-  const { id } = useParams();
-  const { showProperty, property } = useGlobalContext();
+	const { id } = useParams();
+	const { showProperty, property } = useGlobalContext();
 
-  // Load Data
-  useEffect(() => {
-    showProperty(id);
-  }, []);
+	// Load Data
+	useEffect(() => {
+		showProperty(id);
+	}, []);
 
-  return (
-    <>
-      <div className="container">
-        <div className="row">
-          {/* Property Image */}
-          <div className="col">
-            <img
-              className="img-fluid"
-              src={property.immagine}
-              alt={property.immagine}
-            />
-          </div>
-
-          <Badge tipologia={property.tipologia} icona={property.icona}></Badge>
-
-          {/* Property Details */}
-          <div className="col">
-            <h1>{property.nome}</h1>
-            <p>{property.indirizzo}</p>
-            <p>{property.descrizione}</p>
-            <p>{property.numero_like}</p>
-            <div>
-              <ReviewList reviews={property.recensioni} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+					{/* Property Image */}
+					<div className="col-6 immagine-dettaglio">
+						<img
+							className="img-fluid "
+							src={property.immagine}
+							alt={property.immagine}
+						/>
+						<div className="badge-dettaglio">
+							<Badge
+								tipologia={property.tipologia}
+								icona={property.icona}
+							></Badge>
+						</div>
+						{/* Heart */}
+						<span
+							className="position-absolute badge rounded-pill heart-pill text-bg-light fs-6 fw-semibold me-2"
+							// onClick={handleHeartClick}
+						>
+							<i className="fa-solid fa-heart fa-2xl heart" />
+							&nbsp;
+							{property.numero_like}
+						</span>
+					</div>
+					<div className="col-6">
+						<h1>{property.nome}</h1>
+						<p>{property.indirizzo}</p>
+						<p>{property.descrizione}</p>
+					</div>
+				</div>
+				{/* Property Details */}
+				<div className="row mt-2">
+					<div>
+						<ReviewList reviews={property.recensioni} />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
