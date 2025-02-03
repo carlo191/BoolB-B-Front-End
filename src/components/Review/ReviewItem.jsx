eca0f263c87f9a7a648258e476791bb8642683d6
AndreaBeltramin <andreabeltra99@gmail.com>
2025-02-03 11:24:11 +0100

    fix review item padding

--- src/components/Review/ReviewItem.jsx
+++ src/components/Review/ReviewItem.jsx
@@ -2,36 +2,36 @@
 import ProfileIcon from "../profileIcon/profileIcon";
 
 export default function ReviewItem({ review }) {
-  const generateStars = (rating) => {
-    const stars = [];
-    for (let i = 0; i < 5; i++) {
-      const star =
-        i < rating ? (
-          <i className="fa-solid fa-star" key={i}></i>
-        ) : (
-          <i className="fa-regular fa-star" key={i}></i>
-        );
-      stars.push(star);
-    }
-    return stars;
-  };
+	const generateStars = (rating) => {
+		const stars = [];
+		for (let i = 0; i < 5; i++) {
+			const star =
+				i < rating ? (
+					<i className="fa-solid fa-star" key={i}></i>
+				) : (
+					<i className="fa-regular fa-star" key={i}></i>
+				);
+			stars.push(star);
+		}
+		return stars;
+	};
 
-  return (
-    <div className="col-12 d-flex flex-column flex-sm-row border rounded-4 p-3 mb-3">
-      <div className="user-text flex-grow-1">
-        <h5 className="d-flex align-items-center">
-          <ProfileIcon name={review.nome_utente} />
-          {review.nome_utente}
-        </h5>
+	return (
+		<div className="col-12 d-flex flex-column flex-sm-row border rounded-4 p-2 mb-3">
+			<div className="user-text flex-grow-1">
+				<h5 className="d-flex align-items-center">
+					<ProfileIcon name={review.nome_utente} />
+					{review.nome_utente}
+				</h5>
 
-        <div>{review.contenuto}</div>
-      </div>
+				<div>{review.contenuto}</div>
+			</div>
 
-      <div className="vote">
-        <h6 className="review-vote">
-          Voto: {generateStars(review.voto).map((star) => star)}
-        </h6>
-      </div>
-    </div>
-  );
+			<div className="vote">
+				<h6 className="review-vote">
+					Voto: {generateStars(review.voto).map((star) => star)}
+				</h6>
+			</div>
+		</div>
+	);
 }
