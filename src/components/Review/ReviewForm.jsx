f3cebc8002d5337eca24a964ee55bf86e8571835
AndreaBeltramin <andreabeltra99@gmail.com>
2025-02-04 10:45:19 +0100

    fix messaggi

--- src/components/Review/ReviewForm.jsx
+++ src/components/Review/ReviewForm.jsx
@@ -1,136 +1,161 @@
 import { useState, useEffect } from "react";
-
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { Modal } from "bootstrap";
 // Global variables
 import { useGlobalContext } from "../../context/GlobalContext";
-
 export default function ReviewForm() {
-  const { storeReview, property } = useGlobalContext();
-  const [formData, setFormData] = useState({
-    id_immobile: "",
-    nome_utente: "",
-    contenuto: "",
-    voto: "",
-    data: "",
-    durata: "",
-  });
-
-  useEffect(() => {
-    if (property && property.id) {
-      setFormData((prevFormData) => ({
-        ...prevFormData,
-        id_immobile: property.id,
-      }));
-    }
-  }, [property]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    storeReview(formData);
-    setFormData({
-      id_immobile: "",
-      nome_utente: "",
-      contenuto: "",
-      voto: "",
-      data: "",
-      durata: "",
-    });
-  }
-
-  function handleChange(e) {
-    setFormData((formData) => ({
-      ...formData,
-      [e.target.name]:
-        e.target.name === "voto" ? parseInt(e.target.value) : e.target.value,
-    }));
-  }
-
-  return (
-    <div className="border rounded-5 my-5 p-4">
-      <h2 className="mb-3">
-        <i className="fa-solid fa-pencil fa-md me-2"></i>
-        Scrivi la tua recensione:
-      </h2>
-
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <div className="mb-3">
-          {/* Username Input */}
-          <label htmlFor="nome_utente" className="form-label">
-            Username:
-          </label>
-          <input
-            name="nome_utente"
-            type="text"
-            className="form-control"
-            id="nome_utente"
-            value={formData.nome_utente}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
-        {/* Description Input */}
-        <div className="mb-3">
-          <label htmlFor="contenuto" className="form-label">
-            Testo:
-          </label>
-          <textarea
-            name="contenuto"
-            className="form-control"
-            id="contenuto"
-            rows="3"
-            value={formData.contenuto}
-            onChange={(e) => handleChange(e)}
-          ></textarea>
-        </div>
-        {/* Vote Input */}
-        <div className="mb-3">
-          <label htmlFor="voto" className="form-label">
-            Voto:
-          </label>
-          <input
-            name="voto"
-            type="number"
-            min="1"
-            max="5"
-            className="form-control"
-            id="voto"
-            value={formData.voto}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
-
-        <div className="mb-3">
-          <label className="active" htmlFor="data">
-            Data di soggiorno
-          </label>
-          <input
-            className="form-control"
-            type="date"
-            id="data"
-            name="data"
-            value={formData.data}
-            max={new Date().toISOString().split("T")[0]}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="durata" className="form-label">
-            Durata soggiorno:
-          </label>
-          <input
-            name="durata"
-            type="number"
-            min="1"
-            className="form-control"
-            id="durata"
-            value={formData.durata}
-            onChange={(e) => handleChange(e)}
-          />
-        </div>
-
-        {/* Submit */}
-        <button type="submit" className="btn btn-primary">
-          Aggiungi recensione
-        </button>
-      </form>
-    </div>
-  );
+	const { storeReview, property } = useGlobalContext();
+	const [formData, setFormData] = useState({
+		id_immobile: "",
+		nome_utente: "",
+		contenuto: "",
+		voto: "",
+		data: "",
+		durata: "",
+	});
+	useEffect(() => {
+		if (property && property.id) {
+			setFormData((prevFormData) => ({
+				...prevFormData,
+				id_immobile: property.id,
+			}));
+		}
+	}, [property]);
+	function handleSubmit(e) {
+		e.preventDefault();
+		storeReview(formData);
+		// Trova la modale e mostralo
+		const modalElement = document.getElementById("exampleModalReview");
+		if (modalElement) {
+			const modal = new Modal(modalElement);
+			modal.show();
+		}
+		setFormData({
+			id_immobile: "",
+			nome_utente: "",
+			contenuto: "",
+			voto: "",
+			data: "",
+			durata: "",
+		});
+	}
+	function handleChange(e) {
+		setFormData((formData) => ({
+			...formData,
+			[e.target.name]:
+				e.target.name === "voto" ? parseInt(e.target.value) : e.target.value,
+		}));
+	}
+	return (
+		<div className="border rounded-5 p-4">
+			<h2 className="mb-3">
+				<i className="fa-solid fa-pencil fa-md me-2"></i>
+				Scrivi la tua recensione:
+			</h2>
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<div className="mb-3">
+					{/* Username Input */}
+					<label htmlFor="nome_utente" className="form-label">
+						Username:
+					</label>
+					<input
+						required
+						name="nome_utente"
+						type="text"
+						className="form-control"
+						placeholder="Scrivici il tuo nome"
+						id="nome_utente"
+						value={formData.nome_utente}
+						onChange={(e) => handleChange(e)}
+					/>
+				</div>
+				{/* Description Input */}
+				<div className="mb-3">
+					<label htmlFor="contenuto" className="form-label">
+						Testo:
+					</label>
+					<textarea
+						name="contenuto"
+						className="form-control"
+						id="contenuto"
+						placeholder="Scrivici come è stata la tua esperienza"
+						rows="3"
+						value={formData.contenuto}
+						onChange={(e) => handleChange(e)}
+					></textarea>
+				</div>
+				{/* Vote Input */}
+				<div className="mb-3">
+					<label htmlFor="voto" className="form-label">
+						Voto:
+					</label>
+					<input
+						required
+						name="voto"
+						type="number"
+						min="1"
+						max="5"
+						placeholder="Inserisci un voto da 1 a 5"
+						className="form-control"
+						id="voto"
+						value={formData.voto}
+						onChange={(e) => handleChange(e)}
+					/>
+				</div>
+				<div className="mb-3">
+					<label className="active form-label" htmlFor="data">
+						Data di soggiorno:
+					</label>
+					<input
+						required
+						className="form-control"
+						type="date"
+						id="data"
+						name="data"
+						value={formData.data}
+						max={new Date().toISOString().split("T")[0]}
+						onChange={(e) => handleChange(e)}
+					/>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="durata" className="form-label">
+						Durata del soggiorno:
+					</label>
+					<input
+						required
+						name="durata"
+						type="number"
+						min="1"
+						className="form-control"
+						placeholder="Inserisci il n. di giorni della tua permanenza"
+						id="durata"
+						value={formData.durata}
+						onChange={(e) => handleChange(e)}
+					/>
+				</div>
+				{/* Submit */}
+				<button type="submit" className="btn btn-primary">
+					Aggiungi recensione
+				</button>
+				<div className="modal fade" id="exampleModalReview" tabindex="-1">
+					<div className="modal-dialog">
+						<div className="modal-content">
+							<div className="modal-body">
+								La recensione è stata aggiunta con successo!
+							</div>
+							<div className="modal-footer">
+								<button
+									type="button"
+									className="btn btn-secondary"
+									data-bs-dismiss="modal"
+								>
+									Chiudi
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
 }
--- src/components/formMessage/formMessage.jsx
+++ src/components/formMessage/formMessage.jsx
@@ -1,135 +1,168 @@
 import React, { useState } from "react";
-
+import { Modal } from "bootstrap";
 export default function FormMessage() {
-  const [formData, setFormData] = useState({
-    email: "",
-    message: "",
-  });
-
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (formData.email === "" || formData.message === "") {
-      alert("Inserisci una email e un messaggio");
-      return;
-      setIsSubmitted(true);
-      setShowModal(true);
-      setFormData({ email: "", message: "" });
-    }
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  return (
-    <>
-      {/* <button className="btn btn-primary" type="submit">
-					Invia messaggio
-				</button> */}
-
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Contatta il proprietario
-      </button>
-
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-body">
-              <form className=" p-4" onSubmit={handleSubmit}>
-                <div className="mb-3 mt-3">
-                  <input
-                    value={formData.email}
-                    onChange={handleChange}
-                    name="email"
-                    type="email"
-                    className="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Inserisci la tua email"
-                  />
-                </div>
-                <div className="mb-3">
-                  <textarea
-                    value={formData.message}
-                    onChange={handleChange}
-                    name="message"
-                    className="form-control"
-                    id="exampleFormControlTextarea1"
-                    rows="5"
-                    placeholder="Scrivi un messaggio al proprietario"
-                  ></textarea>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-bs-dismiss="modal"
-                    onClick={handleCloseModal}
-                  >
-                    Annulla
-                  </button>
-                  <button type="submit" className="btn btn-primary">
+	const [formData, setFormData] = useState({
+		email: "",
+		message: "",
+	});
+	const [isSubmitted, setIsSubmitted] = useState(false);
+	const [showModal, setShowModal] = useState(false);
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (formData.email === "" || formData.message === "") {
+			alert("Inserisci una email e un messaggio");
+			return;
+		}
+		setIsSubmitted(true);
+		setShowModal(true);
+		// Trova la modale e mostralo
+		const modalElement = document.getElementById("exampleModalFormMessage");
+		if (modalElement) {
+			const modal = new Modal(modalElement);
+			modal.show();
+		}
+		setFormData({ email: "", message: "" });
+	};
+	const handleCloseModal = () => {
+		setShowModal(false);
+		// Chiudi la modale attualmente aperta
+		const modalElement = document.getElementById("exampleModal");
+		if (modalElement) {
+			const modal = Modal.getInstance(modalElement);
+			if (modal) {
+				modal.hide();
+			}
+		}
+	};
+	return (
+		<>
+			{/* <button className="btn btn-primary" type="submit">
                     Invia messaggio
-                  </button>
+                </button> */}
+			<button
+				type="button"
+				className="btn btn-primary"
+				data-bs-toggle="modal"
+				data-bs-target="#exampleModal"
+			>
+				Contatta il proprietario
+			</button>
+			<div
+				className="modal fade"
+				id="exampleModal"
+				tabIndex="-1"
+				aria-labelledby="exampleModalLabel"
+				aria-hidden="true"
+			>
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<button
+								type="button"
+								className="btn-close"
+								data-bs-dismiss="modal"
+								aria-label="Close"
+							></button>
+						</div>
+						<div className="modal-body">
+							<form className=" p-4" onSubmit={handleSubmit}>
+								<div className="mb-3 mt-3">
+									<input
+										required
+										value={formData.email}
+										onChange={handleChange}
+										name="email"
+										type="email"
+										className="form-control"
+										id="exampleFormControlInput1"
+										placeholder="Inserisci la tua email"
+									/>
+								</div>
+								<div className="mb-3">
+									<textarea
+										required
+										value={formData.message}
+										onChange={handleChange}
+										name="message"
+										className="form-control"
+										id="exampleFormControlTextarea1"
+										rows="5"
+										placeholder="Scrivi un messaggio al proprietario"
+									></textarea>
+								</div>
+								<div className="modal-footer">
+									<button
+										type="button"
+										className="btn btn-secondary"
+										data-bs-dismiss="modal"
+										onClick={handleCloseModal}
+									>
+										Annulla
+									</button>
+									<button type="submit" className="btn btn-primary">
+										Invia messaggio
+									</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="modal fade " id="exampleModalFormMessage" tabindex="-1">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-body">
+							Il messaggio è stato inviato con successo!
+						</div>
+						<div className="modal-footer">
+							<button
+								type="button"
+								className="btn btn-secondary"
+								data-bs-dismiss="modal"
+							>
+								Chiudi
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* {showModal && (
+                <div
+                    className="modal fade show"
+                    style={{ display: "block" }}
+                    tabindex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    aria-label="Close"
+                                    onClick={handleCloseModal}
+                                ></button>
+                            </div>
+                            <div className="modal-body">
+                                {isSubmitted ? (
+                                    <h4>
+                                        Il tuo messaggio è stato inviato correttamente, il
+                                        proprietario ti risponderà al più presto!
+                                        <i className="fa-regular fa-face-smile"></i>
+                                    </h4>
+                                ) : (
+                                    <h4>Inserisci una email e un messaggio</h4>
+                                )}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {showModal && (
-        <div
-          className="modal fade show"
-          style={{ display: "block" }}
-          tabindex="-1"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button
-                  type="button"
-                  className="btn-close"
-                  aria-label="Close"
-                  onClick={handleCloseModal}
-                ></button>
-              </div>
-
-              <div className="modal-body">
-                {isSubmitted ? (
-                  <h4>
-                    Il tuo messaggio è stato inviato correttamente, il
-                    proprietario ti risponderà al più presto!
-                    <i className="fa-regular fa-face-smile"></i>
-                  </h4>
-                ) : (
-                  <h4>Inserisci una email e un messaggio</h4>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+            )} */}
+		</>
+	);
 }
