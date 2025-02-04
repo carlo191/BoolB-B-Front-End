8f798cc919b1c2c419fcc91fc2a542daaae53ee7
PieStw <piero.stalteri@icloud.com>
2025-02-04 09:36:16 +0100

    fix

--- src/components/Review/ReviewItem.jsx
+++ src/components/Review/ReviewItem.jsx
@@ -2,46 +2,49 @@
 import ProfileIcon from "../profileIcon/profileIcon";
 
 export default function ReviewItem({ review }) {
-	const generateStars = (rating) => {
-		const stars = [];
-		for (let i = 0; i < 5; i++) {
-			const star =
-				i < rating ? (
-					<i className="fa-solid fa-star" key={i}></i>
-				) : (
-					<i className="fa-regular fa-star" key={i}></i>
-				);
-			stars.push(star);
-		}
-		return stars;
-	};
+  const generateStars = (rating) => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      const star =
+        i < rating ? (
+          <i className="fa-solid fa-star" key={i}></i>
+        ) : (
+          <i className="fa-regular fa-star" key={i}></i>
+        );
+      stars.push(star);
+    }
+    return stars;
+  };
 
-	return (
-		<div className="col-12 d-flex flex-column flex-sm-row border rounded-4 p-2 mb-3">
-			<div className="user-text flex-grow-1">
-				<h5 className="d-flex align-items-center">
-					<ProfileIcon name={review.nome_utente} />
-					{review.nome_utente}
-				</h5>
+  return (
+    <div className="col-12 d-flex flex-column flex-sm-row border rounded-4 p-2 mb-3">
+      <div className="user-text flex-grow-1">
+        <h5 className="d-flex align-items-center">
+          <ProfileIcon name={review.nome_utente} />
+          {review.nome_utente}
+        </h5>
 
-				<div>{review.contenuto}</div>
-			</div>
-			<div>
-				<div className="vote">
-					<h6 className="review-vote">
-						Voto: {generateStars(review.voto).map((star) => star)}
-					</h6>
-				</div>
-				<div>
-					<h6>Data di soggiorno: {review.data_soggiorno.split("T", 1)}</h6>
-				</div>
-				<div>
-					<h6>
-						Durata: {review.numero_giorni}{" "}
-						{review.numero_giorni == 1 ? "giorno" : "giorni"}{" "}
-					</h6>
-				</div>
-			</div>
-		</div>
-	);
+        <div>{review.contenuto}</div>
+      </div>
+      <div>
+        <div className="vote">
+          <h6 className="review-vote">
+            Voto: {generateStars(review.voto).map((star) => star)}
+          </h6>
+        </div>
+        <div>
+          <h6>
+            Data di soggiorno:{" "}
+            {new Date(review.data_soggiorno).toISOString().split("T")[0]}
+          </h6>
+        </div>
+        <div>
+          <h6>
+            Durata: {review.numero_giorni}{" "}
+            {review.numero_giorni == 1 ? "giorno" : "giorni"}{" "}
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
 }
