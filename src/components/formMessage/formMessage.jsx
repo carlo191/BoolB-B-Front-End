79fb6a92ec35ee725f2558b105509b61ca37c6cf
AndreaBeltramin <andreabeltra99@gmail.com>
2025-02-04 11:01:47 +0100

    fix messaggio

--- src/components/formMessage/formMessage.jsx
+++ src/components/formMessage/formMessage.jsx
@@ -53,6 +53,14 @@ export default function FormMessage() {
 			>
 				<div className="modal-dialog">
 					<div className="modal-content">
+						<div className="modal-header">
+							<button
+								type="button"
+								className="btn-close"
+								data-bs-dismiss="modal"
+								aria-label="Close"
+							></button>
+						</div>
 						<div className="modal-body">
 							<form className=" p-4" onSubmit={handleSubmit}>
 								<div className="mb-3 mt-3">
